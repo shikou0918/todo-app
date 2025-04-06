7ba6ad153456d455a0d5d4f1794da9e6e4ce7ef3
shikou0918 <shikou.0918@gmail.com>
2025-04-06 23:00:00 +0900

    components追加

---
+++ frontend/src/components/TodoItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-list-item>
+    <v-list-item-action>
+      <v-checkbox
+        :input-value="todo.completed"
+        @change="$emit('toggle-todo', todo.id)"
+        color="primary"
+      ></v-checkbox>
+    </v-list-item-action>
+
+    <v-list-item-content>
+      <v-list-item-title
+        :class="{ 'text-decoration-line-through': todo.completed }"
+      >
+        {{ todo.text }}
+      </v-list-item-title>
+    </v-list-item-content>
+
+    <v-list-item-action>
+      <v-btn icon @click="$emit('delete-todo', todo.id)">
+        <v-icon color="red lighten-1">mdi-delete</v-icon>
+      </v-btn>
+    </v-list-item-action>
+  </v-list-item>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+@Component
+export default class TodoItem extends Vue {
+  @Prop({ required: true }) todo!: Todo;
+}
+</script>
---
+++ frontend/src/components/TodoList.vue
@@ -0,0 +1,27 @@
+
+<script lang="ts">
+
+/** Todoリスト */
+interface TodoList {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+</script>
+<template>
+  <div>
+    <v-list v-if="todos.length > 0">
+      <TodoItem
+        v-for="todo in todos"
+        :key="todo.id"
+        :todo="todo"
+        @delete-todo="$emit('delete-todo', todo.id)"
+        @toggle-todo="$emit('toggle-todo', todo.id)"
+      />
+    </v-list>
+    <v-alert v-else type="info" text class="mt-3">
+      TODOがありません。新しいTODOを追加してください。
+    </v-alert>
+  </div>
+</template>
